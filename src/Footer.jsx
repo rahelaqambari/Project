60cf2b3ec61ba269010049adb87a9bf35bfec478
rahelaqambari <rahelaqambari@gmail.com>
2025-11-08 19:52:48 +0430

    footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
         <h1 className='text-3xl uppercase text-white '>Follow us </h1>
         <p className='w-[100vh] text-center pb-10'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta corrupti aliquam nam voluptatem culpa, mollitia animi provident, maiores deserunt numquam tempora recusandae architecto omnis, nisi magni consequuntur rem. Ut, dolore.</p>
       <div className='border-t border-t-white w-[90%] h-fit flex items-center justify-center'>
-        <h1 className='text-3xl pt-4'>2025 &copy; copy right Rahela Qambari</h1>
+        <h1 className='text-3xl pt-4'>2025 &copy; Designing by Rahela Qambari</h1>
       </div>
     </div>
   )
