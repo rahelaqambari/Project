bea877c6f8d78bc509c5a79323448f0a1b05e46c
rahelaqambari <rahelaqambari@gmail.com>
2025-11-10 20:25:56 +0430

    some thing is changed

--- src/Box.tsx
+++ src/Box.tsx
@@ -3,8 +3,9 @@ import React from 'react'
 function Box() {
   return (
     <div className='h-screen w-full flex justify-center items-center'>
-      <div className='w-[40%] h-[50%] border border-white outline-1 outline-cyan-800 bg-cyan-700 rounded-2xl flex justify-center items-center'> 
-        <h1 className='text-white text-2xl font-bold'>Welcome to The <span className='text-3xl '>SANABAHAR</span> Studio</h1>
+      <div className='w-[40%] h-[50%] border border-white outline-1 outline-cyan-800 bg-cyan-700 rounded-2xl flex flex-col justify-center items-center'> 
+        <h1 className='text-4xl text-white font-bold bg-gradient-to-l'>SANABAHAR</h1>
+        <h1 className='text-white text-2xl font-bold'>Welcome to The Sanabahar Studio</h1>
       </div>
     </div>
   )
