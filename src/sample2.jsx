350378903b36b0749b4943652c3b42c9b74513dd
rahelaqambari <rahelaqambari@gmail.com>
2026-01-14 22:13:48 +0430

    style

---
+++ src/sample2.jsx
@@ -0,0 +1,10 @@
+function sample2() {
+  return (
+    <div className="w-full min-h-screen bg-linear-150 from-gray-200 to-gray-600 flex flex-col items-center ">
+        <div className="p-24 bg-white w-7/12">
+        <h1 className="text-3xl font-bold">Learning Prisama</h1></div>
+    </div>
+  )
+}
+
+export default sample2
