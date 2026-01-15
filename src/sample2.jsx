64a5c72283cfdc17f41d0a3429678f95a28a00a0
rahelaqambari <rahelaqambari@gmail.com>
2026-01-15 21:31:11 +0430

    date and time

--- src/sample2.jsx
+++ src/sample2.jsx
@@ -2,11 +2,17 @@ function Sample2() {
   return (
     <div className="w-full min-h-screen bg-linear-150 from-gray-400 to-gray-900 flex flex-col items-center ">
         <div className="p-12 bg-white w-7/12 mt-12 rounded-2xl border-2">
-        <h1 className="text-3xl font-bold">Learning Prisama</h1></div>
+        <h1 className="text-3xl font-bold">Learning Prisama</h1>
+        <p className="text-right font-medium">{new Date().toDateString()}</p>
+        </div>
         <div className="p-12 bg-white w-7/12 mt-12 rounded-2xl border-2">
-        <h1 className="text-3xl font-bold">Doing Homework</h1></div>
+        <h1 className="text-3xl font-bold">Doing Homework</h1>
+        <p className="text-right font-medium">{new Date().toDateString()}</p>
+        </div>
         <div className="p-12 bg-white w-7/12 mt-12 rounded-2xl border-2">
-        <h1 className="text-3xl font-bold">Cheaking Emails</h1></div>
+        <h1 className="text-3xl font-bold">Cheaking Emails</h1>
+        <p className="text-right font-medium">{new Date().toDateString()}</p>
+        </div>
     </div>
   )
 }
