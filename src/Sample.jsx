f5d0f5f659681209d1b934db7ae5039a92d3fada
rahelaqambari <rahelaqambari@gmail.com>
2026-01-17 20:21:10 +0430

    changes

--- src/Sample.jsx
+++ src/Sample.jsx
@@ -1,13 +1,14 @@
 import React, { useRef } from 'react'
 
 function Sample() {
-  const input = useRef();
+  const input = useRef('');
   const hndelClick = ()=>{
     input.current.focus();
   }
   return (
-    <div className='w-full min-h-screen flex items-center justify-center '>
-      <input type="text" ref={input} />
+    <div className='w-full min-h-screen flex items-center justify-center flex-col gap-3 '>
+      <input className='p-4 outline-0 border rounded-md w-7/12' type="text" ref={input} />
+      <button className='text-base font-bold py-2 px-4 rounded-md bg-blue-600' onClick={hndelClick}>Change</button>
       {/* <div className='h-32 w-32 rounded-full bg-linear-to-r from-0% to-green-800 animate-spin border-8 text-clip bg-transparent text-3xl'></div> */}
     </div>
   )
