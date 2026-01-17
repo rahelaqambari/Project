0cfbd9aa3cf6c4deb40ad11ec656fbc6ca523b56
rahelaqambari <rahelaqambari@gmail.com>
2026-01-17 20:16:06 +0430

    UseRef

--- src/Sample.jsx
+++ src/Sample.jsx
@@ -1,9 +1,13 @@
 import React, { useRef } from 'react'
 
 function Sample() {
-  const data = useRef();
+  const input = useRef();
+  const hndelClick = ()=>{
+    input.current.focus();
+  }
   return (
     <div className='w-full min-h-screen flex items-center justify-center '>
+      <input type="text" ref={input} />
       {/* <div className='h-32 w-32 rounded-full bg-linear-to-r from-0% to-green-800 animate-spin border-8 text-clip bg-transparent text-3xl'></div> */}
     </div>
   )
