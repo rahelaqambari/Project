b03d759be4cc0f04d7ab526601524db167173abc
rahelaqambari <rahelaqambari@gmail.com>
2026-02-06 20:50:29 +0430

    changes

--- src/Sample.jsx
+++ src/Sample.jsx
@@ -7,10 +7,11 @@ function Sample() {
   }
   return (
     <div className='w-full min-h-screen flex items-center justify-center flex-col gap-3 '>
+      <h1 className='text-4xl font-serif text-blue-600'> Create an acount</h1>
       <input className='p-4 focus:outline-0 border focus:border-0 focus:ring-1 focus:ring-blue-600 rounded-md w-7/12' type="text" ref={input} placeholder='Enter Your Name' />
       <input className='p-4 focus:outline-0 border focus:border-0 focus:ring-1 focus:ring-blue-600 rounded-md w-7/12' type="text" ref={input} placeholder='Enter Your LastName' />
       <input className='p-4 focus:outline-0 border focus:border-0 focus:ring-1 focus:ring-blue-600 rounded-md w-7/12' type="text" ref={input} placeholder='Enter Your Email' />
-      <button className='font-bold py-2 px-4 text-2xl focus:-transition-3 duration-150 rounded-md bg-blue-600 text-white' onClick={hndelClick}>Create Acount</button>
+      <button className='font-bold py-2 px-4 text-2xl focus:-transition-3 duration-150 rounded-md bg-blue-600 text-white' onClick={hndelClick}>Save</button>
       {/* <div className='h-32 w-32 rounded-full bg-linear-to-r from-0% to-green-800 animate-spin border-8 text-clip bg-transparent text-3xl'></div> */}
     </div>
   )
