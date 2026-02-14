0492b0317ba63cb73629d01b5ae59129c9c1c21e
rahelaqambari <rahelaqambari@gmail.com>
2026-02-14 20:13:17 +0430

    bg-linear

--- src/example.jsx
+++ src/example.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Example() {
   return (
     <div className='min-h-screen w-full flex justify-center'>
-      <h1></h1>
+      <h1 className='text-4xl bg-linear-120 from-blue-400 to-green-500 via-orange-600 bg-transparent text-clip'>Try agian</h1>
     </div>
   )
 }
