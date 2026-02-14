e572a0f69baca5715e140bc2e4ef863cc9494216
rahelaqambari <rahelaqambari@gmail.com>
2026-02-14 20:22:24 +0430

    changes

--- src/example.jsx
+++ src/example.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Example() {
   return (
-    <div className='min-h-screen w-full flex items-center justify-center'>
-      <h1 className='text-4xl bg-linear-120 from-blue-400 to-green-500 via-orange-600 p-4 animate-bounce text-transparent bg-clip-text font-bold '>Try agian</h1>
-      <button>Try</button>
+    <div className='min-h-screen w-full flex flex-col items-center justify-center'>
+      <h1 className='text-4xl bg-linear-120 from-blue-400 to-green-500 via-orange-600 p-4 animate-bounce text-transparent bg-clip-text font-bold '> Never Give Up 🎓🏆</h1>
+      <button className='bg-green-600 py-3 px-8 text-white font-bold'>Try</button>
     </div>
   )
 }
