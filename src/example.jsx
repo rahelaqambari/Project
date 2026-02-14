ae99451479531d3ad73f9b64874f85af303be1d0
rahelaqambari <rahelaqambari@gmail.com>
2026-02-14 20:11:12 +0430

    page is added

---
+++ src/example.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Example() {
+  return (
+    <div className='min-h-screen w-full flex justify-center'>
+      <h1></h1>
+    </div>
+  )
+}
+
+export default Example
