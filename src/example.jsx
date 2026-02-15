f00857fdd733ea1a97855c3da0dda88dd64c2d7e
rahelaqambari <rahelaqambari@gmail.com>
2026-02-15 11:49:02 +0430

    useref

--- src/example.jsx
+++ src/example.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 function Example() {
+   const text = useRef("");
+   function change (){
+    text.current.textContent = "Now You Are A Hero"
+   }
   return (
     <div className='min-h-screen w-full flex flex-col items-center justify-center'>
-      <h1 className='text-4xl bg-linear-120 from-blue-400 to-green-500 via-orange-600 p-4 animate-bounce text-transparent bg-clip-text font-bold '> Never Give Up 🎓🏆</h1>
-      <button className='bg-green-600 py-3 px-8 text-white font-bold'>Try</button>
+      <h1 ref={text} className='text-4xl bg-linear-120 from-blue-400 to-green-500 via-orange-600 p-4 animate-bounce text-transparent bg-clip-text font-bold '> Never Give Up 🎓🏆</h1>
+      <button onClick={change} className='bg-green-600 py-3 px-8 text-white font-bold'>Try</button>
     </div>
   )
 }
