22717440854c4b7e2a78bd0962931a6ac00c0a98
rahelaqambari <rahelaqambari@gmail.com>
2026-02-24 22:44:21 -0800

    changes

--- src/addpro.jsx
+++ src/addpro.jsx
@@ -1,11 +1,12 @@
 export default function Addpro(){
     return(
         <div className="h-screen w-full flex items-center justify-center">
-            <div className="w-10/12 felx col-span-2 border rounded-md">
+            <div className="w-10/12 felx col-3 border rounded-md">
             <div className="col-sapn-1">
-                <img className="" src="/img/157073039.jpg" alt="" />
+                <h1 className="absolute">Add New Product</h1>
+                <img className="relative" src="/img/157073039.jpg" alt="" />
             </div>
-            <div>
+            <div className="col-span-2">
                 <form action=""></form>
             </div>
             </div>
