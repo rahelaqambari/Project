011ddd9ce29642b20a58b102be7872b63592a76f
rahelaqambari <rahelaqambari@gmail.com>
2026-03-03 08:49:51 -0800

    add new changes

--- src/form3.jsx
+++ src/form3.jsx
@@ -1,15 +1,15 @@
 export default function Form3(){
     return(
-      <div className="w-full h-screen flex flex-col items-center justify-center">
+      <div className="w-full h-screen flex flex-col items-center justify-center gap-3">
         <h1 className="text-3xl font-bold">ADD NEW PRODUCT</h1>
          <form action="" className="flex flex-col gap-6 w-9/12 bg-stone-100">
-      <div className="grid gap-1 grid-cols-2 border foucuse:outline-0">
-        <input type="text" placeholder="Product Name" />
-        <input type="text" placeholder="Product slug" />
+      <div className="grid gap-1 grid-cols-2">
+        <input className=" border foucuse:outline-0 py-2 rounded-sm" type="text" placeholder="Product Name" />
+        <input className=" border foucuse:outline-0 py-2 rounded-sm" type="text" placeholder="Product slug" />
       </div>
       <div className="grid gap-1 grid-cols-2">
-        <input type="text" placeholder="Product price" />
-        <input type="text" placeholder="Product stock" />
+        <input className=" border foucuse:outline-0 py-2 rounded-sm" type="text" placeholder="Product price" />
+        <input className=" border foucuse:outline-0 py-2 rounded-sm" type="text" placeholder="Product stock" />
       </div>
       <div className="grid gap-1 grid-cols-2">
         <select name="category" id="">
@@ -18,10 +18,10 @@ export default function Form3(){
       </div>
       <div className="grid grid-cols-2 gap-1">
         <div className="w-full flex flex-col gap-2">
-          <input type="text" name="" id="" placeholder="Product brand"  />
+          <input className=" border foucuse:outline-0 py-2 rounded-sm" type="text" name="" id="" placeholder="Product brand"  />
           <div className="flex justify-between gap-2 flex-wrap">
-            <input type="file" accept="image/*" />
-            <input type="file" accept="image/*" />
+            <input className=" border foucuse:outline-0 py-2 rounded-sm" type="file" accept="image/*" />
+            <input className=" border foucuse:outline-0 py-2 rounded-sm" type="file" accept="image/*" />
           </div>
         </div>
         <div>
