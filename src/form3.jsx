bfde1a91bfff38ea5c9aa5c6e05834933c26663c
rahelaqambari <rahelaqambari@gmail.com>
2026-03-04 07:13:08 -0800

    new changes

--- src/form3.jsx
+++ src/form3.jsx
@@ -2,7 +2,7 @@ export default function Form3(){
     return(
       <div className="w-full h-screen flex flex-col items-center justify-center gap-3">
         <h1 className="text-3xl font-bold">ADD NEW PRODUCT</h1>
-         <form action="" className="flex flex-col gap-6 w-9/12 bg-stone-100 p-6 rounded-md">
+         <form action="" className="flex flex-col gap-6 w-10/12 bg-stone-100 p-6 rounded-md">
       <div className="grid gap-1 grid-cols-2">
         <input className=" border foucuse:outline-0 py-2 rounded-sm" type="text" placeholder="Product Name" />
         <input className=" border foucuse:outline-0 py-2 rounded-sm" type="text" placeholder="Product slug" />
@@ -15,6 +15,7 @@ export default function Form3(){
         <select className=" border foucuse:outline-0 py-2 rounded-sm" name="category" id="">
         </select>
               <select className=" border foucuse:outline-0 py-2 rounded-sm" name="category" id="">
+                Cousmic Items
         </select>
         
         
@@ -23,12 +24,12 @@ export default function Form3(){
         <div className="w-full flex flex-col gap-2">
           <input className=" border foucuse:outline-0 py-2 rounded-sm" type="text" name="" id="" placeholder="Product brand"  />
           <div className="flex justify-between gap-2 flex-wrap">
-            <input className=" border foucuse:outline-0 py-2 rounded-sm w-full" type="file" accept="image/*" />
-            <input className=" border foucuse:outline-0 py-2 rounded-sm w-full" type="file" accept="image/*" />
+            <input className=" border foucuse:outline-0 py-2 rounded-sm " type="file" accept="image/*" />
+            <input className=" border foucuse:outline-0 py-2 rounded-sm " type="file" accept="image/*" />
           </div>
         </div>
         <div>
-          <textarea className="w-full h-full resize-none border foucuse:outline-0 py-2 rounded-sm" name="description" id=""></textarea>
+          <textarea placeholder="Description" className="px-2 w-full h-full resize-none border foucuse:outline-0 py-2 rounded-sm" name="description" id=""></textarea>
         </div>
       </div>
       <div className="flex items-center justify-center">
