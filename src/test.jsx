4ca03e691b1755de16540267a746f32dd762a57e
rahelaqambari <rahelaqambari@gmail.com>
2026-04-03 20:26:15 +0430

    page

---
+++ src/test.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Testing() {
+  return (
+    <div className='h-screen w-full flex items-center justify-center'>
+      
+    </div>
+  )
+}
+
+export default Testing
