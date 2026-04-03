1a6e967eb1155144b09f6e73ebc8b3e97f5ec4e5
rahelaqambari <rahelaqambari@gmail.com>
2026-04-03 20:29:17 +0430

    changes

--- src/test.jsx
+++ src/test.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 function Testing() {
   return (
     <div className='h-screen w-full flex items-center justify-center'>
-      
+        <h1>Rajester Form</h1>
+      <form action="">
+        <label>Name</label>
+        <input type="text" />
+        <label>Last Name</label>
+        <input type="text" />
+        <label>Phon Number</label>
+        <input type="text" />
+        <input type="submit" />
+      </form>
     </div>
   )
 }
