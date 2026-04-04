a6cd4703f928d0dcf27d86046fc11738a10c849e
rahelaqambari <rahelaqambari@gmail.com>
2026-04-04 21:34:06 +0430

    style

--- src/test.jsx
+++ src/test.jsx
@@ -4,7 +4,7 @@ function Testing() {
   return (
     <div className='h-screen w-full flex items-center justify-center gap-4'>
         <h1>Rajester Form</h1>
-      <form action="">
+      <form action="" className="w-9/12 border shadow sahdow-2xl bg-gray-200 rounded-md">
         <label className='py-2px-4 text-xl font-bold'>Name</label>
         <input className='border outline:0 rounded-md p-4' type="text" />
         <label className='py-2px-4 text-xl font-bold'>Last Name</label>
