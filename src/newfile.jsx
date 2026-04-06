350edf92d54e3d69d842c02441de9a812b7cd809
rahelaqambari <rahelaqambari@gmail.com>
2026-04-06 20:31:46 +0430

    new file

---
+++ src/newfile.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div className='w-full flex col-sapn-4'>
+        <div className='col-span-1 flex flex-col gap-2'>
+
+        </div>
+        {/* hero */}
+        <div className='col-sapn-3'></div>
+      
+    </div>
+  )
+}
+
+export default Dashboard
