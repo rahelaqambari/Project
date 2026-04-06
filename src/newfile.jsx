c38b07cccc85423c57298f496ad359a925f59a87
rahelaqambari <rahelaqambari@gmail.com>
2026-04-06 20:35:46 +0430

    changes

--- src/newfile.jsx
+++ src/newfile.jsx
@@ -3,8 +3,14 @@ import React from 'react'
 function Dashboard() {
   return (
     <div className='w-full flex col-sapn-4'>
-        <div className='col-span-1 flex flex-col gap-2'>
-
+        <div className='col-span-1 flex flex-col gap-2'>\
+            <div>
+                <h1 className='p-4 border boeder-b-2xl font-serif text-2xl'>Dashboard</h1>
+            </div>
+            <a href="">Manager</a>
+            <a href="">Teacher</a>
+            <a href="">Student</a>
+            <a href="">Parants</a>
         </div>
         {/* hero */}
         <div className='col-sapn-3'></div>
