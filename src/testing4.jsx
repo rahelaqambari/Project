e06d2fd89dc1fddfda5198715a9e741cdfbafaf8
rahelaqambari <rahelaqambari@gmail.com>
2026-05-01 21:16:02 +0430

    p and style

--- src/testing4.jsx
+++ src/testing4.jsx
@@ -5,9 +5,10 @@ function Testing4() {
     <div className='w-full h-screen flex items-center justify-center bg-stone-100'>
         <div className='w-full py-6 px-10 flex gap-4'>
             <video src=""></video>
-            <div className='flex items-start'>
-                <h1>Views:</h1>
-                <h1>Rating:</h1>
+            <div className='flex flex-col items-start'>
+                <h1>Views:65</h1>
+                <h1>Rating:120</h1>
+                <p className='w-full text-gray-300'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione accusamus omnis, suscipit minus id, voluptates dignissimos voluptatibus magnam eligendi voluptas perspiciatis sequi quibusdam earum commodi nesciunt ipsum, reprehenderit qui? Veniam.</p>
             </div>
         </div>
       
