bc2e7a7f29de20d69e7af51d591e17b5c1dde734
rahelaqambari <rahelaqambari@gmail.com>
2026-05-04 23:07:02 +0430

    changes

--- src/testing4.jsx
+++ src/testing4.jsx
@@ -10,6 +10,16 @@ function Testing4() {
                 <h1>Rating:120</h1>
                 <p className='w-full text-gray-300'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione accusamus omnis, suscipit minus id, voluptates dignissimos voluptatibus magnam eligendi voluptas perspiciatis sequi quibusdam earum commodi nesciunt ipsum, reprehenderit qui? Veniam.</p>
             </div>
+              <div className='flex flex-col items-start'>
+                <h1>Views:65</h1>
+                <h1>Rating:120</h1>
+                <p className='w-full text-gray-300'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione accusamus omnis, suscipit minus id, voluptates dignissimos voluptatibus magnam eligendi voluptas perspiciatis sequi quibusdam earum commodi nesciunt ipsum, reprehenderit qui? Veniam.</p>
+            </div>
+              <div className='flex flex-col items-start'>
+                <h1>Views:65</h1>
+                <h1>Rating:120</h1>
+                <p className='w-full text-gray-300'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione accusamus omnis, suscipit minus id, voluptates dignissimos voluptatibus magnam eligendi voluptas perspiciatis sequi quibusdam earum commodi nesciunt ipsum, reprehenderit qui? Veniam.</p>
+            </div>
         </div>
       
     </div>
