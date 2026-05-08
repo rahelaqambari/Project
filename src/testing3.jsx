0a7dd24dc0229a243087f9d92b05b4f46e01d535
rahelaqambari <rahelaqambari@gmail.com>
2026-05-08 21:08:05 +0430

    use state

--- src/testing3.jsx
+++ src/testing3.jsx
@@ -4,7 +4,10 @@ function Test3() {
     const [paly, setplay] = useState();
   return (
     <div className='w-full flex h-screen'>
-      
+        <video controls width={300}>
+            <source src='app.css'/>
+        </video>
+      <button onClick={setplay. reting+2, views+1}>Play</button>
     </div>
   )
 }
