419e84125f441845e155e2e929af736d008d3f33
rahelaqambari <rahelaqambari@gmail.com>
2026-05-08 21:05:42 +0430

    new changes

---
+++ src/testing3.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+function Test3() {
+    const [paly, setplay] = useState();
+  return (
+    <div className='w-full flex h-screen'>
+      
+    </div>
+  )
+}
+
+export default Test3
