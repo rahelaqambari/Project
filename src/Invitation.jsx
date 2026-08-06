921782bfab142bcc560a952ecefc63eedfde2a41
rahelaqambari <rahelaqambari@gmail.com>
2026-08-06 21:20:24 +0430

    changes

--- src/Invitation.jsx
+++ src/Invitation.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 function Invitation() {
   return (
     <div className='h-full w-full flex items-center justify-center'>
-        <div className='w-5/12 border h-full'></div>
+        <div className='w-5/12  h-full relative'>
+          <img className='h-full w-full blur-xs' src="/img/1-3.jpeg" alt="" />
+          <h1 className='text-2xl absolute top-1 left-40 '>By the Name of Allah </h1>
+          <h1 className='text-2xl absolute top-12 left-20 font-serif'>Love Is The Frist Reason For the Life </h1>
+        </div>
       
     </div>
   )
