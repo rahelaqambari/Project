5d550beb45c2260e634ffad89738758ae6d407c5
rahelaqambari <rahelaqambari@gmail.com>
2026-08-06 21:09:00 +0430

    working on style

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@
 // import Clock from "./Clock"
 // import Form from "./Form"
 // import Homework from "./Homework"
-import Imggallary from "./Imggallary"
+// import Imggallary from "./Imggallary"
 // import Login from "./Login"
 // import Memo from "./Memo"
 // import Portfolio from "./Portfolio"
@@ -17,13 +17,15 @@ import Imggallary from "./Imggallary"
 // import Box from "./Box"
 // import Ref from "./Ref"
 
+import Invitation from "./Invitation"
+
 // import Form3 from "./form3"
 // import Dashboard from "./newfile"
 
 // import Addpro from "./addpro"
 
-import Footer2 from "./Footer2"
-import Raheefooter from "./raheefooter"
+// import Footer2 from "./Footer2"
+// import Raheefooter from "./raheefooter"
 // import Header from "./Header"
 // import Sample2 from "./sample2"
 
@@ -36,9 +38,9 @@ function App() {
       {/* <Addpro /> */}
      {/* <Header />
       <Sample2 /> */}
-      <Footer2 />
+      {/* <Footer2 /> */}
       {/* <Sample /> */}
-      <Imggallary />
+      {/* <Imggallary /> */}
       {/* <Ref /> */}
       {/* <State /> */}
       {/* <Box /> */}
@@ -57,7 +59,8 @@ function App() {
       {/* <Transition />    */}
       {/* <Dashboard /> */}
       {/* <Footerm /> */}
-      <Raheefooter />
+      {/* <Raheefooter /> */}
+      <Invitation />
     </div> 
   )
 }
---
+++ src/Invitation.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Invitation() {
+  return (
+    <div className='h-full w-full flex items-center justify-center'>
+        <div className='w-5/12 border h-full'></div>
+      
+    </div>
+  )
+}
+
+export default Invitation
