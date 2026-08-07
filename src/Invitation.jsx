64c521dd8abcd4c354b2c979dafe61bed2f7adbc
rahelaqambari <rahelaqambari@gmail.com>
2026-08-07 22:49:48 +0430

    new changes

--- src/Invitation.jsx
+++ src/Invitation.jsx
@@ -7,6 +7,9 @@ function Invitation() {
           <img className='h-full w-full blur-xs' src="/img/1-3.jpeg" alt="" />
           <h1 className='text-2xl absolute top-1 left-40 '>By the Name of Allah </h1>
           <h1 className='text-2xl absolute top-12 left-20 font-serif'>Love Is The Frist Reason For the Life </h1>
+          <h1 className='text-2xl absolute top-48 left-32 font-serif'>Mr. Ali Rahimi</h1>
+          <h1 className='text-2xl absolute top-52 left-40 font-serif'>and </h1>
+          <h1 className='text-2xl absolute top-72 left-60 font-serif'>Mrs. Freshta Qambari </h1>
         </div>
       
     </div>
