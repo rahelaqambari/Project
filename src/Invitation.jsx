6478137d92b095634b87b5db611105f2d366bb7f
rahelaqambari <rahelaqambari@gmail.com>
2026-08-08 22:08:28 +0430

    new changes

--- src/Invitation.jsx
+++ src/Invitation.jsx
@@ -7,9 +7,13 @@ function Invitation() {
           <img className='h-full w-full blur-xs' src="/img/1-3.jpeg" alt="" />
           <h1 className='text-2xl absolute top-1 left-40 '>By the Name of Allah </h1>
           <h1 className='text-2xl absolute top-12 left-20 font-serif'>Love Is The Frist Reason For the Life </h1>
-          <h1 className='text-2xl absolute top-48 left-32 font-serif'>Mr. Ali Rahimi</h1>
-          <h1 className='text-2xl absolute top-52 left-40 font-serif'>and </h1>
-          <h1 className='text-2xl absolute top-72 left-60 font-serif'>Mrs. Freshta Qambari </h1>
+          <h1 className='text-2xl absolute top-28 left-18 font-serif'>Mr. Ali Rahimi</h1>
+          <h1 className='text-2xl absolute top-40 left-62 font-serif'>and </h1>
+          <h1 className='text-2xl absolute top-52 left-60 font-serif'>Mrs. Fatima Raha</h1>      
+          <h1 className='text-2xl absolute top-72 left-3 font-serif'>Mr/Mrs  ___________ Join Us to Celebrith The Best and Lovely Day of the year</h1>           
+          <h1 className='text-2xl absolute bottom-80 left-3 font-serif'>🗓️ 12/8/2025</h1>           
+          <h1 className='text-2xl absolute bottom-72 left-3 font-serif'>Location: Qasri Saltanatiy </h1>           
+          <h1 className='text-2xl absolute bottom-64 left-3 font-serif'>You are Invited By: Ramazan</h1>           
         </div>
       
     </div>
