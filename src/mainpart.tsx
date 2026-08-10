b5c24e1db5b9215fb477cfa4b3bf563681eddddd
rahelaqambari <rahelaqambari@gmail.com>
2026-08-10 21:36:12 +0430

    new page

---
+++ src/mainpart.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MainPart() {
+  return (
+    <div className='h-screen w-full flex items-center justify-center'>
+      <div className='w-7/12 h-80 border rounded-md'>
+      <h1>Design the Best Dresses Here</h1></div>
+    </div>
+  )
+}
+
+export default MainPart
