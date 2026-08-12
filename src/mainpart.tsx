c1f11cfcc043656a46a5bce91265d9dac23f272f
rahelaqambari <rahelaqambari@gmail.com>
2026-08-12 21:22:02 +0430

    new changes

--- src/mainpart.tsx
+++ src/mainpart.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 function MainPart() {
   return (
-    <div className='h-screen w-full flex items-center justify-center'>
-      <div className='w-7/12 h-80 border rounded-md'>
-      <h1>Design the Best Dresses Here</h1></div>
+    <div className='h-screen w-full flex items-center bg-black justify-center'>
+      <div className='w-7/12 h-80 border rounded-md border-white border-collapse'>
+      <h1>Design the Best Dresses Here</h1>
+      <p className='w-full p-8 text-sm'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero eius expedita culpa eligendi vero. Molestias aliquid dolore animi sequi veritatis cupiditate commodi autem, aut illo vitae culpa neque ipsam nostrum?</p>
+      </div>
     </div>
   )
 }
